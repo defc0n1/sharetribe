9df23288924f76426d2bff464d53e6f8a61ecbc3
Kimmo Puputti <kimmo@sharetribe.com>
2016-09-16 14:16:08 +0300

    Handle UUIDTools:UUID instances in Transit

--- app/utils/transit_utils.rb
+++ app/utils/transit_utils.rb
@@ -1,9 +1,27 @@
 module TransitUtils
+
+  class UUIDWriteHandler
+    def tag(_)
+      "u"
+    end
+
+    def rep(u)
+      Transit::UUID.new(u.to_s)
+    end
+
+    def string_rep(u)
+      u.to_s
+    end
+  end
+
   module_function
 
   def encode(content, encoding)
     io = StringIO.new('', 'w+')
-    writer = Transit::Writer.new(encoding, io)
+
+    writer = Transit::Writer.new(encoding, io, handlers: {
+                                   UUIDTools::UUID => UUIDWriteHandler.new
+                                 })
     writer.write(content)
     io.string
   end
